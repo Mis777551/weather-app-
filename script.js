3f3326de8650c3d5851de321c234016cb0209f65
vidimamisokuhle <vidimamisokuhle@gmail.com>
2025-06-06 22:03:11 +0200

    added date and  time description element

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ function updateWeatherUI(data) {
   document.querySelector(".weather-icon").setAttribute("alt", data.condition.description);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function getWeather(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(function (response) {
